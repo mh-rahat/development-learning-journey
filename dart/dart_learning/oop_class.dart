e2db4705ac34faa59a251dfb1d60290440438722
mh-rahat <mhrahat765@gmail.com>
2025-07-17 15:01:37 +0600

    add the oop_class.dart file

--- dart/dart_learning/oop_class.dart
+++ dart/dart_learning/oop_class.dart
@@ -1,1 +1,14 @@
 // Class means a structure, blueprint, or model used to create objects.
+
+class Addition {
+  add() {
+    int a = 20, b = 50, c;
+    c = a + b;
+    print(c);
+  }
+}
+
+void main() {
+  Addition obj = Addition();
+  obj.add();
+}
