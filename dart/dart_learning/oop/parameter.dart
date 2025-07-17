516dec9ee2b91e2b5be5fbfb242be3919968c3c4
mh-rahat <mhrahat765@gmail.com>
2025-07-17 16:27:39 +0600

    add file and restore file

--- dart/dart_learning/oop/parameter.dart
+++ dart/dart_learning/oop/parameter.dart
@@ -1,3 +1,27 @@
 // Parameter
-  // 1. Positional Parameter 
-  // 2. Named Parameter
+// 1. Positional Parameter
+// 2. Named Parameter
+
+// positional parameter
+class Test1 {
+  add(int num1, int num2) {
+    int sum = num1 + num2;
+    print(sum);
+  }
+}
+
+// named parameter
+class Test2 {
+  add({required int num1, required int num2}) {
+    int sum = num1 + num2;
+    print(sum);
+  }
+}
+
+void main() {
+  Test1 obj1 = Test1();
+  obj1.add(40, 60);
+
+  Test2 obj2 = Test2();
+  obj2.add(num1: 50, num2: 100);
+}
