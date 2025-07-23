e3a732cd2a399063859057a14fea090de3c0df0f
mh-rahat <mhrahat765@gmail.com>
2025-07-24 00:12:29 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/widgets/container_page.dart
+++ dart/flutter_learning/flutter_basics/lib/widgets/container_page.dart
@@ -7,12 +7,22 @@ class ContainerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Container Page')),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          color: Colors.blueGrey,
-          child: Text('This is Container Page!'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.all(Radius.circular(21)),
+              border: Border.all(width: 3, color: Colors.black),
+              boxShadow: [
+                BoxShadow(blurRadius: 21, color: Colors.grey, spreadRadius: 11),
+              ],
+            ),
+          ),
         ),
       ),
     );
