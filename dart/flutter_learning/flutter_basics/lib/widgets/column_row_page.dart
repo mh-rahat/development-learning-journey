c3ac081de7ca68c71b67051e2a5fc1b5b36abdb1
mh-rahat <mhrahat765@gmail.com>
2025-07-23 18:32:33 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/widgets/column_row_page.dart
+++ dart/flutter_learning/flutter_basics/lib/widgets/column_row_page.dart
@@ -4,7 +4,31 @@ class ColumnRowWidgetPage extends StatelessWidget {
   const ColumnRowWidgetPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Column & Row Page')),
+    return Scaffold(
+      appBar: AppBar(title: Text('Column & Row Page')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('A', style: TextStyle(fontSize: 25)),
+          Text('B', style: TextStyle(fontSize: 25)),
+          Text('C', style: TextStyle(fontSize: 25)),
+          Text('D', style: TextStyle(fontSize: 25)),
+          Text('E', style: TextStyle(fontSize: 25)),
+          ElevatedButton(onPressed: () {}, child: Text('Click')),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('R1', style: TextStyle(fontSize: 25)),
+              Text('R2', style: TextStyle(fontSize: 25)),
+              Text('R3', style: TextStyle(fontSize: 25)),
+              Text('R4', style: TextStyle(fontSize: 25)),
+              Text('R5', style: TextStyle(fontSize: 25)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
