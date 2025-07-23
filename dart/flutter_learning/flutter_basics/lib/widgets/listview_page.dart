0634628b37efad770572db6ed89ef1361d218f08
mh-rahat <mhrahat765@gmail.com>
2025-07-23 22:11:28 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/widgets/listview_page.dart
+++ dart/flutter_learning/flutter_basics/lib/widgets/listview_page.dart
@@ -4,6 +4,49 @@ class ListviewWidgetPage extends StatelessWidget {
   const ListviewWidgetPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Listview Page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Listview Page')),
+      body: Center(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'One',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Two',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Three',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Four',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Five',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
