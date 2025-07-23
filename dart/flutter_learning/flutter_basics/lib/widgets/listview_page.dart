dac39d5773f4815f1e39404f766b47d8daed678a
mh-rahat <mhrahat765@gmail.com>
2025-07-23 23:13:29 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/widgets/listview_page.dart
+++ dart/flutter_learning/flutter_basics/lib/widgets/listview_page.dart
@@ -4,48 +4,29 @@ class ListviewWidgetPage extends StatelessWidget {
   const ListviewWidgetPage({super.key});
   @override
   Widget build(BuildContext context) {
+    var arrNames = [
+      'Rahim',
+      'Karim',
+      'Ramjan',
+      'Bijoy',
+      'Shanto',
+      'Rifat',
+      'Siam',
+      'Hridoy',
+    ];
     return Scaffold(
       appBar: AppBar(title: Text('Listview Page')),
-      body: Center(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'One',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Two',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Three',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Four',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Five',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
-              ),
-            ),
-          ],
-        ),
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return Text(
+            arrNames[index],
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+          );
+        },
+        itemCount: arrNames.length,
+        separatorBuilder: (context, index) {
+          return Divider(height: 100, thickness: 4);
+        },
       ),
     );
   }
