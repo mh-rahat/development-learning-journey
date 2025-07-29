18dfba7b034983598b3f628e9bd3621598a2c52c
mh-rahat <mhrahat765@gmail.com>
2025-07-29 12:39:59 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/widgets/date_time_page.dart
+++ dart/flutter_learning/flutter_basics/lib/widgets/date_time_page.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basics/ui_helper/util.dart';
 
-class DateTimeWidgetPage extends StatelessWidget {
+class DateTimeWidgetPage extends StatefulWidget {
   const DateTimeWidgetPage({super.key});
+
+  @override
+  State<DateTimeWidgetPage> createState() => _DateTimeWidgetPageState();
+}
+
+class _DateTimeWidgetPageState extends State<DateTimeWidgetPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Date & Time Page')),
       body: Center(
-        child: Container(
+        child: SizedBox(
           width: 200,
           height: 200,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('Current Time: $time'),
-         
+            children: [
+              Text(
+                'Current Time: ${time.hour}:${time.minute}:${time.second}',
+                style: TextStyle(fontSize: 25),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    time = DateTime.now();
+                  });
+                },
+                child: Text('Current Time'),
+              ),
             ],
           ),
         ),
