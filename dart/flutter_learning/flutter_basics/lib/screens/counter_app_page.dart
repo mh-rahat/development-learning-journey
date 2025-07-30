cf5dfebe5e72480830df60949637b450b702badf
mh-rahat <mhrahat765@gmail.com>
2025-07-30 23:22:34 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/screens/counter_app_page.dart
+++ dart/flutter_learning/flutter_basics/lib/screens/counter_app_page.dart
@@ -8,11 +8,40 @@ class CounterAppPage extends StatefulWidget {
 }
 
 class _CounterAppPageState extends State<CounterAppPage> {
+  var count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Counter App')),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Count: $count', style: TextStyle(fontSize: 34)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                // count = count + 1;
+                // count += 1;
+
+                setState(() {
+                  count++;
+                  debugPrint('Count');
+                });
+              },
+              child: Text(
+                'Increment Counter',
+                style: TextStyle(color: Colors.white70),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
