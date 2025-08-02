f0879615de65254aa90a6cb3603a9d5ff0ecdb26
mh-rahat <mhrahat765@gmail.com>
2025-08-02 14:22:03 +0600

    update the file

--- dart/flutter_learning/flutter_basics/lib/screens/ripple_effect_animation_page.dart
+++ dart/flutter_learning/flutter_basics/lib/screens/ripple_effect_animation_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-class RippleEffectAnimationPage extends StatelessWidget {
+class RippleEffectAnimationPage extends StatefulWidget {
   const RippleEffectAnimationPage({super.key});
+
+  @override
+  State<RippleEffectAnimationPage> createState() => _RippleEffectAnimationPageState();
+}
+
+class _RippleEffectAnimationPageState extends State<RippleEffectAnimationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('Ripple Effect Animation Page')), 
-    body: Container(),
+    body: Container(
+      
+    ),
     );
   }
 }
