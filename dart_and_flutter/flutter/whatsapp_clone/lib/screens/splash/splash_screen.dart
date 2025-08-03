834569d365d603881e148c30336ee633ef5f4017
mh-rahat <mhrahat765@gmail.com>
2025-08-03 17:03:08 +0600

    Splash Screen file

--- dart_and_flutter/flutter/whatsapp_clone/lib/screens/splash/splash_screen.dart
+++ dart_and_flutter/flutter/whatsapp_clone/lib/screens/splash/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(Duration(seconds: 2), () {
+    Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
