d2e7d42bd234c6c37ac21e0080eea726c67777f7
mh-rahat <mhrahat765@gmail.com>
2025-08-06 11:55:04 +0600

    update the chats screen file

--- dart_and_flutter/flutter/whatsapp_clone/lib/screens/home/chats/chats_screen.dart
+++ dart_and_flutter/flutter/whatsapp_clone/lib/screens/home/chats/chats_screen.dart
@@ -24,7 +24,7 @@ class ChatsScreen extends StatelessWidget {
         'message': '2',
       },
       {
-        'images':
+        'image':
             'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg',
         'name': 'Bjarne Stroustrup',
         'lastmessage': 'how are  you bro?',
@@ -80,7 +80,7 @@ class ChatsScreen extends StatelessWidget {
         'message': '3',
       },
       {
-        'images':
+        'image':
             'https://p3.hippopx.com/preview/1003/434/man-model-beard-long-hair-suit-serious-expression-style-fashion-km-nazrul-islam-thumbnail.jpg',
         'name': 'Ken Thompson',
         'lastmessage': 'how is going your life',
