c384762ca82c2334ecac868941f0afc0132c3e73
mh-rahat <mhrahat765@gmail.com>
2025-08-06 14:47:17 +0600

    update the calls screen file

--- dart_and_flutter/flutter/whatsapp_clone/lib/screens/home/calls/calls_screen.dart
+++ dart_and_flutter/flutter/whatsapp_clone/lib/screens/home/calls/calls_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/ui_helper.dart';
 
 class CallsScreen extends StatelessWidget {
   const CallsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Calls')));
+    var callContent = [
+      {
+        'image':
+            'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTXy1di9MvPCkTqd933oO0Lx5QS1CuSr-2PmH119LI6uqRcrpJpb4pAPY9dhCmBmRfr',
+        'name': 'Bill Gates',
+        'calltime': '1 hour ago',
+      },
+      {
+        'image':
+            'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRXvlD5YAOPgiwNVEF1hIXk_-sxH45qRjpd679YFd9ntZ1_YBpnb6saipxcGs6lMUgM',
+        'name': 'Alan Turing',
+        'calltime': '3 hour ago',
+      },
+    ];
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 20),
+              UiHelper.customText(text: 'Recent', height: 18),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(
+                      callContent[index]['image'].toString(),
+                    ),
+                  ),
+                  title: UiHelper.customText(
+                    text: callContent[index]['name'].toString(),
+                    height: 18,
+                    fontweight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  subtitle: UiHelper.customText(
+                    text: callContent[index]['calltime'].toString(),
+                    height: 16,
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.call, color: Color(0xff00a884)),
+                  ),
+                );
+              },
+              itemCount: callContent.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
