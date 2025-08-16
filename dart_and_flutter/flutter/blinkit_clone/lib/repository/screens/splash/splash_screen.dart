3d81d77aad9e541584666cc3fcb1b441cfaa6f9a
mh-rahat <mhrahat765@gmail.com>
2025-08-16 14:25:10 +0600

    update the file

---
+++ dart_and_flutter/flutter/blinkit_clone/lib/repository/screens/splash/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:blinkit_clone/domain/constants/app_colors.dart';
+import 'package:blinkit_clone/repository/widgets/ui_helper.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UiHelper.CustomImage(image: 'blinkit.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
